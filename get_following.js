775d5a83870d5ff85010687a0cdde6b8a8f4538b
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2021-02-09 00:01:48 -0500

    get_following: add button to fetch full bios

--- get_following.js
+++ get_following.js
@@ -2,10 +2,23 @@ async function loadHandler() {
   if (await doRedirect()) {
     return;
   }
-  const userId = parseInt(location.search.substring(1));
+  const parts = location.search.substring(1).split(',');
+  const userId = parseInt(parts[0]);
+  const getFullBio = parts.length > 1 && parts[1] == 'full';
+  if (getFullBio) {
+    document.getElementById('get-full-bios').style.display = '';
+  } else {
+    document.getElementById('get-full-bios').href =
+        '/get_following.html?' + userId + ',full';
+  }
+
   const response = await apiPost('get_following', {user_id: userId});
   const usersContainer = document.getElementById('users-container');
-  for (const user of response.users) {
+  for (const userRaw of response.users) {
+    const user = getFullBio ? (await apiPost('get_profile', {
+                                user_id: userRaw.user_id
+                              })).user_profile :
+                              userRaw;
     const userLink = document.createElement('a');
     userLink.href = '/user.html?' + user.user_id;
     userLink.innerText = user.username;
